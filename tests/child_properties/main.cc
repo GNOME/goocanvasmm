954777fd8e1081b640a19f4de09356b249060f31
Murray Cumming <murrayc@murrayc.com>
2008-10-08 16:40:19 +0000

    Added a test for get_child_property().
    2008-10-08  Murray Cumming  <murrayc@murrayc.com>
    
    * Makefile.am:
    * configure.in:
    * tests/Makefile.am:
    * tests/child_properties/: Added a test
    for get_child_property().
    
    svn path=/goocanvasmm/trunk/; revision=1734

--- tests/child_properties/main.cc
+++ tests/child_properties/main.cc
@@ -16,18 +16,27 @@
  */
 
 #include <gtkmm.h>
-#include "examplewindow.h"
+#include <goocanvasmm.h>
+#include <iostream>
 
 int
 main(int argc, char* argv[])
 {
-  Gtk::Main app(&argc, &argv);
+  Gtk::Main app(&argc, &argv) ;
   Goocanvas::init("goocanvasmm Simple Example", "0.1", argc, argv);
 
-  ExampleWindow sw;
-  Gtk::Main::run(sw);
+  Glib::RefPtr<Goocanvas::Table> table = Goocanvas::Table::create();
+
+  //Add a child to the table:
+  Glib::RefPtr<Goocanvas::Text> child = Goocanvas::Text::create("test");
+  table->attach(child, 2, 3, 5, 6, Gtk::EXPAND | Gtk::FILL, Gtk::SHRINK);
+
+  //Examine the child property:
+  Glib::Value<int> column_value;
+  column_value.init(Glib::Value<int>::value_type());
+  table->get_child_property(child, "column", column_value);
+
+  std::cout << "Column: " << column_value.get() << std::endl;
 
   return 0;
 }
-
-
